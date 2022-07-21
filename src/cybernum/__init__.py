6a8f425adcf586f17dc532a67ef31d1079339960
むずでょ <muzudho1@gmail.com>
2022-07-21 22:09:30 +0900

    Parse

--- src/cybernum/__init__.py
+++ src/cybernum/__init__.py
@@ -1,9 +1,14 @@
+import re
 from beadsvec import BeadsVec
 from dicordnum import DicOrdNum
 
 
 class CyberNum:
     """Cyber Number"""
+
+    # めんどくさいので .upper() して 'O' と数字で構成されていればOkとする
+    # 辞書順記数法に対応するために、めんどくさいので '_', 'A' が含まれていてもOkとする
+    __pattern1 = re.compile(r"^([_AO\d]*)$")
 
     @staticmethod
     def be_accurate(value=0):
@@ -19,39 +24,83 @@ class CyberNum:
             #
             # タプル型なら
             if type(value) is tuple:
-                # いったんリストに戻して 0 の要素を追加
+                # いったんリストに戻す
                 element_list = list(value)
-                element_list.append(0)
-                # タプルに変換して使う
-                return CyberNum(tuple(element_list))
+
             else:
                 # TODO 整数ではなかった
-                raise ValueError
+
+                # 文字列を解析する
+                element_list = CyberNum.convert_str_to_list(value)
 
         else:
             # 整数だ
-            # リストに変換し、0 の要素を追加
+            # リストに変換する
             element_list = []
             element_list.append(int_value)
-            element_list.append(0)
+
+        # 0 の要素を追加
+        element_list.append(0)
 
         # タプルに変換して使う
         return CyberNum(tuple(element_list))
+
+    @staticmethod
+    def convert_str_to_list(text):
+        # 大文字に変換
+        text = text.upper()
+
+        # '_', 'A', 'O' と数字で構成されている必要がある
+        result = CyberNum.__pattern1.match(text)
+        if result:
+            pass
+        else:
+            raise ValueError(f"not cyber vector: {text}")
+
+        # 先頭に O が付いているのは構わないものとし、
+        # 先頭に付いている O は削除する
+        text = text.lstrip('O')
+
+        new_element_list = []
+
+        # 区切り文字 O で分割
+        tokens = text.split('O')
+
+        for token in tokens:
+            if token[:1] == 'A':
+                # A を除去する
+                n = int(token.replace('A', ''))
+                new_element_list.append(n)
+
+            elif token[:1] == '_':
+                # まず '_' を除去する
+                token = token.replace('_', '')
+
+                figure = len(token)
+                modulo = 1
+                for i in range(0, figure):
+                    modulo *= 10
+
+                z = -1 * (modulo - int(token))
+                new_element_list.append(z)
+
+            else:
+                n = int(token)
+                new_element_list.append(n)
+
+        return new_element_list
 
     def __init__(self, value=0, order=1):
         self._order = order
         self._beadsvec = BeadsVec(value)
 
     def __str__(self):
-        """辞書順記数法"""
+        """辞書順記数法 と 数珠玉記数法 の併用"""
         text = ""
         for token in self.elements:
-            # print(
-            #    f"token:{token} DicOrdNum(token):{DicOrdNum(token)} text:{text}")
             text = f"{text}o{DicOrdNum(token)}"
 
         # 先頭だけを大文字の 'O' にする
-        # print(f"text:{text}")
         text = f"O{text[1:]}"
         return text
 
